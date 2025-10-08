d0abccdf0111239d546c7737ff5447a0133bd512
Arian Sumak <Arian.sumak@pufin.id>
2025-10-08 15:00:06 +0200

    working scripts opefully

---
+++ submit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+### ------------- specify queue name ----------------
+BSUB -q c02516
+### ------------- specify gpu request----------------
+BSUB -gpu “num=1:mode=exclusive_process”
+### ------------- specify job name ----------------
+BSUB -J testjob
+### ------------- specify number of cores ----------------
+BSUB -n 4
+BSUB -R "span[hosts=1]"
+### ------------- specify CPU memory requirements ----------------
+BSUB -R "rusage[mem=20GB]”
+### ------------- specify wall-clock time (max allowed is 12:00)---------------- 
+BSUB -W 12:00
+BSUB -o OUTPUT_FILE%J.out
+BSUB -e OUTPUT_FILE%J.err
+source /zhome/8a/1/224071/try/bin/activate
+python /PATH/TO/PYTHON_FIL
